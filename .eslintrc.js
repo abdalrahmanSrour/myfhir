18cd4d3fc1339ac90ccbeaf625b5477937d442a4
Abed M. Srour <3bd.srour@gmail.com>
2021-08-09 01:33:07 +0300

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
+};
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,17 @@
 
 import chalk from 'chalk';
 import { clear } from 'console';
-import path from 'path';
 import { Command } from 'commander';
 import figlet from 'figlet';
 
 clear();
-console.log(
-    chalk.red(
-        figlet.textSync('myFHIR-cli', { horizontalLayout: 'full' })
-    )
-);
+console.log(chalk.red(figlet.textSync('myFHIR-cli', { horizontalLayout: 'full' })));
 
 const program = new Command();
 
 program
     .version('0.1.0')
-    .description("Generate mySQL schema from FHIR resources definitions")
+    .description('Generate mySQL schema from FHIR resources definitions')
     // .option('-p, --peppers', 'Add peppers')
     // .option('-P, --pineapple', 'Add pineapple')
     // .option('-b, --bbq', 'Add bbq sauce')
