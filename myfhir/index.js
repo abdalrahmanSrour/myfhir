70c01672b4c5ba16b712ad23fefa933e753bcc4f
Abed M. Srour <3bd.srour@gmail.com>
2021-08-21 22:36:03 +0300

    Change build dir

--- myfhir/index.js
+++ myfhir/index.js
@@ -4,21 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var chalk_1 = __importDefault(require("chalk"));
-var console_1 = require("console");
-var commander_1 = require("commander");
-var figlet_1 = __importDefault(require("figlet"));
+const chalk_1 = __importDefault(require("chalk"));
+const console_1 = require("console");
+const commander_1 = require("commander");
+const figlet_1 = __importDefault(require("figlet"));
 console_1.clear();
 console.log(chalk_1.default.red(figlet_1.default.textSync('myFHIR-cli', { horizontalLayout: 'full' })));
-var program = new commander_1.Command();
+const program = new commander_1.Command();
 program
     .version('0.1.0')
-    .description("Generate mySQL schema from FHIR resources definitions")
+    .description('Generate mySQL schema from FHIR resources definitions')
     // .option('-p, --peppers', 'Add peppers')
     // .option('-P, --pineapple', 'Add pineapple')
     // .option('-b, --bbq', 'Add bbq sauce')
     .option('-f, --folder <path>', 'Definition folder path')
     // .option('-C, --no-cheese', 'You do not want any cheese')
     .parse(process.argv);
-var options = program.opts();
+const options = program.opts();
 console.log(options);
