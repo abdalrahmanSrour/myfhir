147df553e5c15927f2591fd8eb17ae4782b74a75
mahmoudshanti <alshantimahmoud40@gmail.com>
2022-06-13 23:59:36 +0300

    Change testing framework from mocha to jest

--- src/loaders/__test__/DirRecursiveLoader.test.ts
+++ src/loaders/__test__/DirRecursiveLoader.test.ts
@@ -1,44 +1,43 @@
 import DirRecursiveLoader from '../DirRecursiveLoader';
-import { expect } from 'chai';
 
 describe('Test DirRecursiveLoader', () => {
     it('Test One Subfile', () => {
         const dirRecursiveLoader = new DirRecursiveLoader(`${__dirname}/FoldersAndFiles`);
         dirRecursiveLoader.load();
-        expect(dirRecursiveLoader.dirents.length, 'Dirents length').to.equal(2);
+        expect(dirRecursiveLoader.dirents.length).toEqual(2);
 
         dirRecursiveLoader.forEach((dirent, index) => {
             if (index === 1) {
-                expect(dirent.isFile()).to.equal(false);
-                expect(dirent.isDirectory()).to.equal(true);
-                expect(dirent.name).to.equal('folder1');
+                expect(dirent.isFile()).toEqual(false);
+                expect(dirent.isDirectory()).toEqual(true);
+                expect(dirent.name).toEqual('folder1');
             } else if (index === 0) {
-                expect(dirent.isFile()).to.equal(true);
-                expect(dirent.isDirectory()).to.equal(false);
-                expect(dirent.name).to.equal('file1');
+                expect(dirent.isFile()).toEqual(true);
+                expect(dirent.isDirectory()).toEqual(false);
+                expect(dirent.name).toEqual('file1');
             }
         });
 
         dirRecursiveLoader.forEachFile((file) => {
-            expect(file).to.be.oneOf([
+            expect([
                 `${__dirname}/FoldersAndFiles/file1`,
                 `${__dirname}/FoldersAndFiles/folder1/file2`,
-            ]);
+            ]).toContain(file);
         });
     });
 
     it('Test Three Level SubFolder', () => {
         const dirRecursiveLoader = new DirRecursiveLoader(`${__dirname}/ThreeLevels`);
         dirRecursiveLoader.load();
-        expect(dirRecursiveLoader.dirents.length, 'Dirents length').to.equal(2);
+        expect(dirRecursiveLoader.dirents.length).toEqual(2);
 
         dirRecursiveLoader.forEachFile((file) => {
-            expect(file).to.be.oneOf([
+            expect([
                 `${__dirname}/ThreeLevels/fileLevel0`,
                 `${__dirname}/ThreeLevels/Level1/fileLevel1`,
                 `${__dirname}/ThreeLevels/Level1/Level2/fileLevel2`,
                 `${__dirname}/ThreeLevels/Level1/Level2/Level3/fileLevel3`,
-            ]);
+            ]).toContain(file);
         });
     });
 });
